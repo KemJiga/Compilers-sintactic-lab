9aa6e9439b6421fd199de943793b2e7051f9c522
Kemjiga <kemerj@uninorte.edu.co>
2023-11-08 22:17:24 -0500

    doc: update test docs

--- test.sh
+++ test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+clear
 yacc -d labs.y
-#lex debugger.l
 lex labs.l
 gcc lex.yy.c y.tab.c -o labs
 
-
-./labs entrada.txt>salida.txt
-./labs entrada2.txt>salida2.txt
+./labs entrada.txt > ./out/salida.txt
+./labs entrada2.txt > ./out/salida2.txt
 
 echo "Ejecucion terminada"
