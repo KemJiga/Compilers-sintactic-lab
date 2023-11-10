54e6e3bc28b12136843c4cdcc3b25d6842029275
Kemjiga <kemerj@uninorte.edu.co>
2023-11-10 01:36:30 -0500

    doc: add test examples

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 clear
-yacc -d labs.y
+yacc -d labs.y 
+#lex debugger.l
 lex labs.l
 gcc lex.yy.c y.tab.c -o labs
 
-./labs entrada.txt > ./out/salida.txt
-./labs entrada2.txt > ./out/salida2.txt
+./labs entrada.txt > salida.txt
+./labs entrada2.txt > salida2.txt
+./labs entrada3.txt > salida3.txt
 
 echo "Ejecucion terminada"
